f73ae66cb7a694943f4338198fa542bf8c808847
kareemsober <soberkareem@gmail.com>
2022-01-29 17:03:43 +0200

    new44

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'sa-east-1_wzkpdlOcg', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4j3hf9ouu2thgs0jlk7h35tdbl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'sa-east-1' // e.g. us-east-2
+        userPoolId: 'us-east-1_Juhh96ipq', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6l8jtnf9heta8sm0rf134hoj1u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://an2eirbqsk.execute-api.sa-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
